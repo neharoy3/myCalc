ed56ce5cd5635e47a98e41741f520913dbf4d616
Neha Roy <neharoy20063@gmail.com>
2026-02-02 02:42:38 +0530

    updated history panel

--- frontend/index.js
+++ frontend/index.js
@@ -1,261 +1,262 @@
 let sessionId = localStorage.getItem("calc_session");
-if(!sessionId){ //generate a unique id if not there
-    sessionId=crypto.randomUUID();
-    localStorage.setItem("calc_session",sessionId);
+if (!sessionId) {
+  //generate a unique id if not there
+  sessionId = crypto.randomUUID();
+  localStorage.setItem("calc_session", sessionId);
 }
 
 //click sound effect!
 const clicksound = new Audio("./media/click.mp3");
-clicksound.volume=0.4;
+clicksound.volume = 0.4;
 
 const historyToggle = document.getElementById("historyToggle");
 const historyPanel = document.getElementById("historyPanel");
+const closeHistoryBtn = document.getElementById("closeHistoryBtn");
+
 historyToggle.addEventListener("click", () => {
-    historyPanel.classList.toggle("active");
+  historyPanel.classList.toggle("active");
 });
+
+if (closeHistoryBtn) {
+  closeHistoryBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    historyPanel.classList.remove("active");
+  });
+}
 
 const display = document.getElementById("display");
 //create history storage
 let historyData = JSON.parse(localStorage.getItem("calcHistory")) || [];
-function saveHistory(expression, result){
+function saveHistory(expression, result) {
+  historyData.unshift({
+    exp: expression,
+    res: result,
+  });
 
-    historyData.unshift({
-        exp: expression,
-        res: result
-    });
+  // keep only last 20 entries
+  if (historyData.length > 20) {
+    historyData.pop();
+  }
 
-    // keep only last 20 entries
-    if(historyData.length > 20){
-        historyData.pop();
-    }
+  localStorage.setItem("calcHistory", JSON.stringify(historyData));
 
-    localStorage.setItem("calcHistory", JSON.stringify(historyData));
+  // send to backend (cloud sync)
+  fetch(`https://mycalc-backend.onrender.com/history/${sessionId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      expression: expression,
+      result: result,
+    }),
+  }).catch((err) => console.log("Cloud save failed:", err));
 
-    // send to backend (cloud sync)
-    fetch(`https://mycalc-backend.onrender.com/history/${sessionId}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            expression: expression,
-            result: result
-        })
-    }).catch(err => console.log("Cloud save failed:", err));
-
-    renderHistory();
+  renderHistory();
 }
 
-function renderHistory(){
+function renderHistory() {
+  historyList.innerHTML = "";
 
-    historyList.innerHTML = "";
+  historyData.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.exp} = ${item.res}`;
 
-    historyData.forEach(item => {
-
-        const li = document.createElement("li");
-        li.textContent = `${item.exp} = ${item.res}`;
-
-        li.addEventListener("click", () => {
-            display.value = item.res;
-        });
-
-        historyList.appendChild(li);
-
+    li.addEventListener("click", () => {
+      display.value = item.res;
     });
+
+    historyList.appendChild(li);
+  });
 }
 
 //clear history
 const clearHistoryBtn = document.getElementById("clearHistoryBtn");
 clearHistoryBtn.addEventListener("click", () => {
+  if (historyData.length === 0) return;
 
-    if(historyData.length === 0) return;
+  const confirmClear = confirm("Clear all history?");
 
-    const confirmClear = confirm("Clear all history?");
+  if (confirmClear) {
+    historyPanel.classList.add("clear-anim");
 
-    if(confirmClear){
-        historyPanel.classList.add("clear-anim");
-
-        setTimeout(() => {
-            historyData = [];
-            localStorage.removeItem("calcHistory");
-            renderHistory();
-            historyPanel.classList.remove("clear-anim");
-        }, 200);
-
-    }
-
+    setTimeout(() => {
+      historyData = [];
+      localStorage.removeItem("calcHistory");
+      renderHistory();
+      historyPanel.classList.remove("clear-anim");
+    }, 200);
+  }
 });
 
-function appendToDisplay(input){
-    clicksound.currentTime=0;
-    clicksound.play();
+function appendToDisplay(input) {
+  clicksound.currentTime = 0;
+  clicksound.play();
 
-    const value  = display.value;
-    const lastChar = value.slice(-1);
-    const operators = "+−×÷";
+  const value = display.value;
+  const lastChar = value.slice(-1);
+  const operators = "+−×÷";
 
-    //prevent starting w/ operator
-    if(operators.includes(input) && value===""){
-        if(input!=="−") return;
+  //prevent starting w/ operator
+  if (operators.includes(input) && value === "") {
+    if (input !== "−") return;
+  }
+
+  //block extra operators
+  if (operators.includes(input) && operators.includes(lastChar)) {
+    return;
+  }
+
+  //block extra decimals
+  if (input == ".") {
+    const value = display.value;
+    const parts = value.split(/[+\-×÷]/);
+    const lastNum = parts[parts.length - 1];
+    if (lastNum.includes(".")) {
+      return;
     }
-
-    //block extra operators
-    if(operators.includes(input) && operators.includes(lastChar)){
-        return;
-    }
-
-    //block extra decimals
-    if(input=="."){
-        const value = display.value;
-        const parts = value.split(/[+\-×÷]/);
-        const lastNum = parts[parts.length-1];
-        if(lastNum.includes(".")){
-            return;
-        }
-    }
-    display.value += input;
+  }
+  display.value += input;
 }
 
-function clearDisplay(playsound=true){
-    if(playsound){
-        clicksound.currentTime=0;
-        clicksound.play();
-    }
-    
-    display.value = "";
+function clearDisplay(playsound = true) {
+  if (playsound) {
+    clicksound.currentTime = 0;
+    clicksound.play();
+  }
+
+  display.value = "";
 }
 
-function calculate(){
-    clicksound.currentTime=0;
-    clicksound.play();
-    try{
-        let expression = display.value;
+function calculate() {
+  clicksound.currentTime = 0;
+  clicksound.play();
+  try {
+    let expression = display.value;
 
-        expression = expression.replace(/×/g, "*");
-        expression = expression.replace(/÷/g, "/");
-        expression = expression.replace(/−/g, "-");
-        //% is percentage 
-        expression = expression.replace(/(\d+\.?\d*)%/g, "($1/100)");
+    expression = expression.replace(/×/g, "*");
+    expression = expression.replace(/÷/g, "/");
+    expression = expression.replace(/−/g, "-");
+    //% is percentage
+    expression = expression.replace(/(\d+\.?\d*)%/g, "($1/100)");
 
-        let result = eval(expression);
+    let result = eval(expression);
 
-        //only 4 decimal places
-        if (!Number.isInteger(result)) {
-            result = parseFloat(result.toFixed(4));
-        }
-
-        display.value = result;
-        saveHistory(expression,result);
+    //only 4 decimal places
+    if (!Number.isInteger(result)) {
+      result = parseFloat(result.toFixed(4));
     }
-    catch{
-        display.value = "INVALID FORMAT";
-        //shake animation
-        display.classList.add("shake");
-        //remove after animation
-        setTimeout(()=>{
-            display.classList.remove("shake");
-        },400);
 
-        //silent clear
-        setTimeout(() => {
-            clearDisplay(false);
-        }, 2000);
-    }
+    display.value = result;
+    saveHistory(expression, result);
+  } catch {
+    display.value = "INVALID FORMAT";
+    //shake animation
+    display.classList.add("shake");
+    //remove after animation
+    setTimeout(() => {
+      display.classList.remove("shake");
+    }, 400);
+
+    //silent clear
+    setTimeout(() => {
+      clearDisplay(false);
+    }, 2000);
+  }
 }
 
 //brackets button
 const bracketBtn = document.getElementById("bracketBtn");
 
 bracketBtn.addEventListener("click", () => {
-    const value = display.value;
-    const lastChar = value.slice(-1);
+  const value = display.value;
+  const lastChar = value.slice(-1);
 
-    const openBrackets = (value.match(/\(/g) || []).length;
-    const closeBrackets = (value.match(/\)/g) || []).length;
+  const openBrackets = (value.match(/\(/g) || []).length;
+  const closeBrackets = (value.match(/\)/g) || []).length;
 
-    if (
-        value === "" ||
-        "+-*/(".includes(lastChar) ||
-        openBrackets === closeBrackets
-    ) {
-        appendToDisplay("(");
-    } else {
-        appendToDisplay(")");
-    }
+  if (
+    value === "" ||
+    "+-*/(".includes(lastChar) ||
+    openBrackets === closeBrackets
+  ) {
+    appendToDisplay("(");
+  } else {
+    appendToDisplay(")");
+  }
 });
 
 const backspaceBtn = document.getElementById("backspaceBtn");
 
 backspaceBtn.addEventListener("click", () => {
-    clicksound.currentTime=0;
-    clicksound.play();
-    display.value = display.value.slice(0, -1);
+  clicksound.currentTime = 0;
+  clicksound.play();
+  display.value = display.value.slice(0, -1);
 });
 
 //keyboard keys input
 document.addEventListener("keydown", (event) => {
-    clicksound.currentTime=0;
-    clicksound.play();
-    const key = event.key;
+  clicksound.currentTime = 0;
+  clicksound.play();
+  const key = event.key;
 
-    if (!isNaN(key) || key === ".") {
-        appendToDisplay(key);
-    }
+  if (!isNaN(key) || key === ".") {
+    appendToDisplay(key);
+  }
 
-    if (key === "+") appendToDisplay("+");
-    if (key === "-") appendToDisplay("−");
-    if (key === "*") appendToDisplay("×");
-    if (key === "/") appendToDisplay("÷");
-    if (key === "%") appendToDisplay("%");
+  if (key === "+") appendToDisplay("+");
+  if (key === "-") appendToDisplay("−");
+  if (key === "*") appendToDisplay("×");
+  if (key === "/") appendToDisplay("÷");
+  if (key === "%") appendToDisplay("%");
 
-    if (key === "Enter") {
-        event.preventDefault(); // stops form reload behavior
-        calculate();
-    }
- 
-    if (key === "Backspace") {
-        display.value = display.value.slice(0, -1);
-    }
+  if (key === "Enter") {
+    event.preventDefault(); // stops form reload behavior
+    calculate();
+  }
 
-    if (key === "Escape") {
-        clearDisplay();
-    }
+  if (key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  }
 
-    if (key === "(" || key === ")") {
-        appendToDisplay(key);
-    }
+  if (key === "Escape") {
+    clearDisplay();
+  }
+
+  if (key === "(" || key === ")") {
+    appendToDisplay(key);
+  }
 });
 
 //input animation
 document.addEventListener("keydown", () => {
-    display.classList.add("active");
-    setTimeout(() => display.classList.remove("active"), 100);
+  display.classList.add("active");
+  setTimeout(() => display.classList.remove("active"), 100);
 });
 document.addEventListener("keydown", (event) => {
+  let key = event.key;
 
-    let key = event.key;
+  // Map keyboard keys to button text
+  if (key === "*") key = "×";
+  if (key === "/") key = "÷";
+  if (key === "-") key = "−";
+  if (key === "Enter") key = "=";
+  if (key === "Backspace") key = "⌫";
+  if (key === "Escape") key = "C";
+  if (key === "(") key = "()";
+  if (key === ")") key = "()";
 
-    // Map keyboard keys to button text
-    if (key === "*") key = "×";
-    if (key === "/") key = "÷";
-    if (key === "-") key = "−";
-    if (key === "Enter") key = "=";
-    if (key === "Backspace") key = "⌫";
-    if (key === "Escape") key = "C";
-    if (key === "(") key = "()";
-    if (key === ")") key = "()";
+  const buttons = document.querySelectorAll("button");
 
-    const buttons = document.querySelectorAll("button");
+  buttons.forEach((btn) => {
+    if (btn.innerText === key) {
+      btn.classList.add("key-press");
 
-    buttons.forEach(btn => {
-        if (btn.innerText === key) {
-            btn.classList.add("key-press");
-
-            setTimeout(() => {
-                btn.classList.remove("key-press");
-            }, 120);
-        }
-    });
+      setTimeout(() => {
+        btn.classList.remove("key-press");
+      }, 120);
+    }
+  });
 });
 
 //theme toggle
@@ -263,61 +264,55 @@ const themeBtn = document.getElementById("themeToggle");
 const themeIcon = themeBtn.querySelector("img");
 // Load saved theme
 if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("light-mode");
-    themeIcon.src = "./media/sun.png";
+  document.body.classList.add("light-mode");
+  themeIcon.src = "./media/sun.png";
 }
 
 // Toggle theme
 themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 
-    document.body.classList.toggle("light-mode");
-
-    if (document.body.classList.contains("light-mode")) {
-        localStorage.setItem("theme", "light");
-        themeIcon.src = "./media/sun.png";
-    } else {
-        localStorage.setItem("theme", "dark");
-        themeIcon.src = "./media/moon.png";
-    }
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+    themeIcon.src = "./media/sun.png";
+  } else {
+    localStorage.setItem("theme", "dark");
+    themeIcon.src = "./media/moon.png";
+  }
 });
 
 //history panel should close automatically
 document.addEventListener("click", (event) => {
+  //ignore if panel is not open
+  if (!historyPanel.classList.contains("active")) return;
 
-    //ignore if panel is not open
-    if (!historyPanel.classList.contains("active")) return;
+  //ignore if click inside history panel
+  if (
+    historyPanel.contains(event.target) ||
+    historyToggle.contains(event.target)
+  ) {
+    return;
+  }
 
-    //ignore if click inside history panel
-    if (
-        historyPanel.contains(event.target) ||
-        historyToggle.contains(event.target)
-    ) {
-        return;
-    }
-
-    //close panel
-    historyPanel.classList.remove("active");
+  //close panel
+  historyPanel.classList.remove("active");
 });
 
-function loadCloudHistory(){
+function loadCloudHistory() {
+  fetch(`https://mycalc-backend.onrender.com/history/${sessionId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length > 0) {
+        historyData = data.map((item) => ({
+          exp: item.expression,
+          res: item.result,
+        }));
 
-    fetch(`https://mycalc-backend.onrender.com/history/${sessionId}`)
-    .then(res => res.json())
-    .then(data => {
-
-        if(data.length > 0){
-            historyData = data.map(item => ({
-                exp: item.expression,
-                res: item.result
-            }));
-
-            renderHistory();
-        }
-
+        renderHistory();
+      }
     })
-    .catch(err => console.log("Cloud load failed:", err));
+    .catch((err) => console.log("Cloud load failed:", err));
 }
-
 
 renderHistory();
 loadCloudHistory();
